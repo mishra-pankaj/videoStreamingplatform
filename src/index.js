3f65087fefd662b0cd38d326c6c527f370320641
mishra-pankaj <immishrapankaj@gmail.com>
2025-06-02 13:41:43 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -6,3 +6,14 @@ dotenv.config({
     path:"./env"
 })
 connectDB()
+const PORT_OG = process.env.PORT || 8000
+.then(()=> {
+    app.listen(PORT_OG,()=>{
+        console.log(`Server is running at port ${PORT_OG}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log(`Error db connection failed!!${err}`);
+    
+})
